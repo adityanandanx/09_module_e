651f705a3d55e3e76837feef081dd4f99351b2ee
Aditya Nandan <nandanaditya985@gmail.com>
2025-08-15 18:57:12 +0530

    fix: 09_module_e

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Link, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/09_module_e" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div>
+            <Link to={"/09_module_e"}>Go to app</Link>
+          </div>
+        }
+      />
+      <Route path="/09_module_e" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
