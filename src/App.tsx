878dca79ab40c6a1ddb11ff0da9f2049a8a1070d
Aditya Nandan <nandanaditya985@gmail.com>
2025-08-15 15:53:21 +0530

    init: tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "./styles/themes.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [images, setImages] = useState<{ url: string; name: string }[]>([]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col gap-2">
+      {/* Image Upload / Sample Loader */}
+      {images.length === 0 ? (
+        <div className="flex flex-col items-center gap-2">
+          <input
+            type="file"
+            multiple
+            accept="image/*"
+            className="p-10 border border-dashed rounded-md"
+            onChange={(e) => {
+              const files = Array.from(e.target.files || []);
+              const res = files.map((file) => ({
+                url: URL.createObjectURL(file),
+                name: file.name.replaceAll("-", " "),
+              }));
+              setImages(res);
+            }}
+          />
+          <button
+            className="bg-orange-500 text-white px-4 py-2 rounded-md cursor-pointer"
+            onClick={() => {
+              setImages([
+                {
+                  url: "/sample/basilique-notre-dame-de-fourviere-lyon.jpg",
+                  name: "Basilique Notre Dame De Fourviere Lyon",
+                },
+                {
+                  url: "/sample/beautiful-view-in-lyon.jpg",
+                  name: "beautiful view in lyon",
+                },
+                {
+                  url: "/sample/place-bellecour-lyon.jpg",
+                  name: "place bellecour lyon",
+                },
+                {
+                  url: "/sample/tour-metalique-lyon.jpg",
+                  name: "tour metalique lyon",
+                },
+              ]);
+            }}
+          >
+            Use sample
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
-export default App
+export default App;
